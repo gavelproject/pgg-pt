b85fff12b6573b4f048bb2a8ae846515ca83d0b6
Igor C. A. de Lima <igorcadelima@gmail.com>
2018-04-21 11:48:51 -0300

    Repeat simulation until no error occurs

--- run_pgg.sh
+++ run_pgg.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-mkdir -p log
+log_dir='log/'
+mkdir -p $log_dir
 ./gradlew build
+
 for (( i=1; i<=$1; i++ )); do
-	echo
-	echo
-	echo "################ Simulation#$i ################"
-	./gradlew -q
+	printf "################ Simulation#$i ################"
+	log_file=$log_dir\pgg-$((i-1)).log
+
+	# Repeat until the simulation does not present failure
+	while true; do
+		./gradlew -q 2>&-
+		if grep -q fail $log_file; then
+			rm $log_file
+		else
+			break
+		fi
+	done
+	echo ' ✔'
 done
