90361db6312bad977342831ee213c48c303c611d
Igor C. A. de Lima <igorcadelima@gmail.com>
2018-04-24 14:08:43 -0300

    Fix bug on retry upon failure
    The newest log file is always pgg-0.log.
    
    Signed-off-by: Igor C. A. de Lima <igorcadelima@gmail.com>

--- run_pgg.sh
+++ run_pgg.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 log_dir='log/'
+log_file=$log_dir\pgg-0.log
 mkdir -p $log_dir
 ./gradlew build
 
 for (( i=1; i<=$1; i++ )); do
-	printf "################ Simulation#$i ################"
-	log_file=$log_dir\pgg-$((i-1)).log
+        printf "################ Simulation#$i ################"
 
-	# Repeat until the simulation does not present failure
-	while true; do
-		./gradlew -q 2>&-
-		if grep -q fail $log_file; then
-			rm $log_file
-		else
-			break
-		fi
-	done
-	echo ' ✔'
+        # Repeat until the simulation does not present failure
+        while true; do
+                # Run and throw stderr away
+                ./gradlew -q 2>&-
+
+                if grep -q fail $log_file; then
+                        rm $log_file
+                else
+                        break
+                fi
+        done
+        echo ' ✔'
 done
